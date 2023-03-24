07dfbaeb51e54a4d9630f5ff6deb46982dd4fd85
DubeyRitik <ritikdubeysuraj@gmail.com>
2023-03-24 20:05:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Import packages
+const express = require("express");
+// const home = require("./routes/home");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+// app.use("/home", home);
+
+app.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "GamifyLife Backend",
+    message: "Backend for gamify life",
+  });
+});
+
+// connection
+// const port = process.env.PORT || 9001;
+app.listen(3000, () => console.log(`Listening to port ${3000}`));
